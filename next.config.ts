c2a54b29cb0d194d4113d48df46e352898fa8abb
Luis Pelaez <e.luis.pelaez@gmail.com>
2026-05-16 19:55:28 +0800

    remove unsupported keys

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  /* config options here */
 };
 
 export default withPayload(nextConfig);
